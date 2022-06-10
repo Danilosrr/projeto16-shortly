ede5650f029de97c4e57f8405eea4b11f849a6ac
Danilo <danilo_srr@hotmail.com>
2022-06-10 02:57:00 -0300

    ranking added

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import signUpRouter from './routes/signUpRouter.js';
 import signInRouter from './routes/signInRouter.js';
 import urlRouter from './routes/urlsRouter.js';
 import usersRouter from './routes/usersRouter.js';
+import rankingRouter from './routes/rankingRouter.js';
 
 const app = express();
 app.use(cors());
@@ -15,6 +16,7 @@ app.use(signUpRouter);
 app.use(signInRouter);
 app.use(urlRouter);
 app.use(usersRouter);
+app.use(rankingRouter);
 
 app.listen(process.env.PORT, () => {
   console.log('Server running on port', process.env.PORT)}
