5dd7ca64bf42df2644a6982d3ed8f3c71f44aadb
Danilo <danilo_srr@hotmail.com>
2022-06-10 03:55:53 -0300

    tables adjust

--- sql/tables.sql
+++ sql/tables.sql
@@ -28,7 +28,7 @@ CREATE TABLE "shortenedUrls" (
 	"url" TEXT NOT NULL,
 	"shortUrl" TEXT NOT NULL UNIQUE,
 	"createdAt" timestamp with time zone NOT NULL DEFAULT NOW(),
-	"views" INT NOT NULL DEFAULT 0
+	"views" INT NOT NULL DEFAULT 0,
 	CONSTRAINT "shortenedUrls_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -40,7 +40,3 @@ CREATE TABLE "shortenedUrls" (
 ALTER TABLE "sessions" ADD CONSTRAINT "sessions_fk0" FOREIGN KEY ("usersId") REFERENCES "users"("id");
 
 ALTER TABLE "shortenedUrls" ADD CONSTRAINT "shortenedUrls_fk0" FOREIGN KEY ("usersId") REFERENCES "users"("id");
-
-
-
-
