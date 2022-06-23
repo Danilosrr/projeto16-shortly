decbef73a694d996ce14f1671e6d8ebd75df90f2
Luana Lessa <lessalsn@gmail.com>
2022-06-22 22:32:41 -0300

    Code review

--- routes/usersRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getUsers } from "../controllers/usersConctroller.js";
-import { verifyToken } from "../middlewares/tokenMiddleware.js";
-
-const usersRouter = Router();
-
-usersRouter.get('/users/:id', verifyToken, getUsers);
-
-export default usersRouter;
